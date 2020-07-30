9c0c6537236a2e9faef10522f4d9856c03aa9a58
Sarah Phillips <snphillips@gmail.com>
2020-07-30 09:13:35 -0400

    optimize images, rezip

--- src/CuratedSets/kolomanMoser.js
+++ src/CuratedSets/kolomanMoser.js
@@ -1,23 +1,28 @@
 
-
-
 let  kolomanMoser = {
   setName: "Koloman Moser",
   machineName: "kolomanMoser",
   images:[
     {
+      id: '18670519',
+      title: "Print, Tapete Masken (Masks Wallpaper), plate 20, in Die Quelle: Flächen",
+      url: "https://collection.cooperhewitt.org/objects/18670519//",
+      imageURL: "https://images.collection.cooperhewitt.org/.jpg",
+      localImageURL: "/meeting-backgrounds/curatedSets/kolomanMoser/18670519.jpg",
+    },
+    {
       id: '18670503',
-      title: "",
+      title: "Print, Bedruckter Stoff Silvanus (Silvanus Printed Fabric), plate 23, in Die Quelle: Flächen Schmuck (The Source: Ornament for Flat Surfaces), 1901;",
       url: "https://collection.cooperhewitt.org/objects/18670503/",
-      imageURL: "https://images.collection.cooperhewitt.org/b.jpg",
+      imageURL: "https://images.collection.cooperhewitt.org/51501_6cf832e47e50246c_b.jpg",
       localImageURL: "/meeting-backgrounds/curatedSets/kolomanMoser/18670503.jpg"
     },
     {
       id: '18670531',
-      title: "PRINT, DIE RECIPROKEN TÄNZERINNEN (RECIPROCAL DANCERS), PLATE 25, IN DIE QUELLE: FLÄCHEN SCHMUCK (THE SOURCE: ORNAMENT FOR FLAT SURFACES), 1901",
+      title: "Print, Die reciproken Tänzerinnen (Reciprocal Dancers), Plate 25, in Die Quelle: Flächen Schmuck (The Source: Ornament for Flat Surfaces), 1901;",
       url: "https://collection.cooperhewitt.org/objects/18670531/",
-      imageURL: "https://images.collection.cooperhewitt.org/b.jpg",
-      localImageURL: "/meeting-backgrounds/curatedSets/kolomanMoser/.jpg",
+      imageURL: "https://images.collection.cooperhewitt.org/93343_b43b767df6cf24c5_b.jpg",
+      localImageURL: "/meeting-backgrounds/curatedSets/kolomanMoser/18670531.jpg",
     },
     {
       id: '18670539',
@@ -41,13 +46,6 @@ let  kolomanMoser = {
       localImageURL: "/meeting-backgrounds/curatedSets/kolomanMoser/18670493.jpg",
     },
     {
-      id: '18670519',
-      title: "Print, Tapete Masken (Masks Wallpaper), plate 20, in Die Quelle: Flächen",
-      url: "https://collection.cooperhewitt.org/objects/18670519//",
-      imageURL: "https://images.collection.cooperhewitt.org/.jpg",
-      localImageURL: "/meeting-backgrounds/curatedSets/kolomanMoser/18670519.jpg",
-    },
-        {
       id: '18670529',
       title: "Print, Donauwellen Wanddekor fur ein Badezimmer (Danube Wave Wall Decoration...",
       url: "https://collection.cooperhewitt.org/objects/18670529/",
