9f96905bfb8bd5602046897783b59d7b8988adde
Sarah Phillips <snphillips@gmail.com>
2020-07-31 22:46:12 -0400

    curated set tab header

--- src/CuratedSetsComponent.js
+++ src/CuratedSetsComponent.js
@@ -39,71 +39,75 @@ export default class CuratedSetsComponent extends Component {
 
       <section className="component">
 
-        <h2>Curated Sets</h2>
+        <a href="#">
+          <div className="curated-set-heading-div">
+            <h2 className="curated-set-heading">Curated Sets</h2>
+          </div>
+        </a>
 
-        <Masonry breakpointCols={breakpointColumnsObj}
-                 className="my-masonry-grid curated-sets-list"
-                 columnClassName="my-masonry-grid_column"
-                 >
+        <div className="curated-set-window">
+
+          <Masonry breakpointCols={breakpointColumnsObj}
+                   className="my-masonry-grid curated-sets-list"
+                   columnClassName="my-masonry-grid_column"
+                   >
 
 
-          {curatedSetsArray.map( (item, index) => {
+            {curatedSetsArray.map( (item, index) => {
 
+            // console.log("curatedSetsArray[0].images[0].imageURL:", curatedSetsArray[0].images[0].imageURL)
 
-          // console.log("curatedSetsArray[0].images[0].imageURL:", curatedSetsArray[0].images[0].imageURL)
+              return(
 
-            return(
-
-              <div key={index + "-card"}
-                   className="curated-set-card card"
-                   value={item.setName}
-                    >
-
-                <div key={index + "-label"}
-                     className="curated-set-label"
+                <div key={index + "-card"}
+                     className="curated-set-card card"
                      value={item.setName}
-                     onClick={ (event) => {
-                      console.log("button value is:",  event.target.value)
-                      // this.props.handleFilterSubmit(event)
-                    }}>
-                     {item.setName}
+                      >
+
+                  <div key={index + "-label"}
+                       className="curated-set-label"
+                       value={item.setName}
+                       onClick={ (event) => {
+                        console.log("button value is:",  event.target.value)
+                        // this.props.handleFilterSubmit(event)
+                      }}>
+                       {item.setName}
+                  </div>
+
+
+                  <img src={item.images[0].localImageURL}
+                       id={"curated-set-cover-image-" + index}
+                       className="curated-set-cover-image"
+                       />
+
+                  <CuratedSetsImageGallery parentState={this.state}
+                                           curatedSetsArray={curatedSetsArray}
+                                           index={index}
+                                           />
+
+                  <a href= {"/meeting-backgrounds/curatedSets/" + item.machineName + ".zip"}  download >
+
+                    <button key={item.setName + "downlad-button"}
+                            className="curated-set-download-button"
+                            type="download"
+                            value={item.setName}
+                            onClick={ (event) => {
+                              console.log("download selected images")
+                              // this.props.zipDownloadFolderCuratedSet(item.setName, index)
+                            }}>
+                            download image set
+
+                    </button>
+                  </a>
+
                 </div>
+              )
+          })}
 
 
-                <img src={item.images[0].localImageURL}
-                     id={"curated-set-cover-image-" + index}
-                     className="curated-set-cover-image"
-                     />
+          </Masonry>
 
-
-
-
-                <CuratedSetsImageGallery parentState={this.state}
-                                         curatedSetsArray={curatedSetsArray}
-                                         index={index}
-                                         />
-
-                <a href= {"/meeting-backgrounds/curatedSets/" + item.machineName + ".zip"}  download >
-
-                  <button key={item.setName + "downlad-button"}
-                          className="curated-set-download-button"
-                          type="download"
-                          value={item.setName}
-                          onClick={ (event) => {
-                            console.log("download selected images")
-                            // this.props.zipDownloadFolderCuratedSet(item.setName, index)
-                          }}>
-                          download image set
-
-                  </button>
-                </a>
-
-              </div>
-            )
-        })}
-
-
-        </Masonry>
+        </div>
 
       </section>
   );
--- src/Instructions.js
+++ src/Instructions.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Instructions() {
   return (
     <div className="component">
-      <h2>Instructions</h2>
+      <h3>Instructions</h3>
       <div className="collection">(instruction on how to upload to various meeting apps)</div>
     </div>
   );
