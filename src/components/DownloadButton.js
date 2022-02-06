93fd736fcc6d2f1eab1a198d471d095dac54e6f3
Sarah Phillips <snphillips@gmail.com>
2022-02-06 18:22:17 -0500

    refactor from class to functional component

--- src/components/DownloadButton.js
+++ src/components/DownloadButton.js
@@ -1,20 +1,13 @@
 import React from "react";
-import "./index.css";
 
-export default class DownloadButton extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {};
-  }
-
-  render() {
-    return (
+export default function DownloadButton(props) {
+  return (
       <button className="download-button">
-        <a href="/meeting-backgrounds/arches.jpeg" download="arches.jpeg">
-          Download Set of Selected Images
+        <a
+          href="/meeting-backgrounds/arches.jpeg"
+          download="arches.jpeg">
+            Download Set of Selected Images
         </a>
       </button>
-    );
-  }
+  );
 }
