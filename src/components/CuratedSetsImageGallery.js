a39a9a4fa543a740983974f5cf5425aa58c9198b
Sarah Phillips <snphillips@gmail.com>
2022-02-08 22:41:54 -0500

    finish  refactor of CuratedSetsImageGallery to functional component

--- src/components/CuratedSetsImageGallery.js
+++ src/components/CuratedSetsImageGallery.js
@@ -1,16 +1,17 @@
 import React, { Component, useState } from "react";
-// import './index.css';
 import _Lodash from "lodash";
 
-// This section contains the curated set images and the "view set" button.
-// The first image from every set IS NOT displayed, as it it displayed
-// as the cover in CuratedSetsComponent.js. The first image is removed by
-// using the _Lodash library's drop method
+/*
+This section contains the curated set images and the "view set" button.
+The first image from every set IS NOT displayed, as it it displayed
+as the cover in CuratedSetsComponent.js. The first image is removed by
+using the _Lodash library's drop method
 
-// This componenet has two states: imagesRevealed & viewButtonMessage.
-// An image set is either expanded (imagesRevealed: true) or collapsed (imagesRevealed: false)
-// The button message (viewButtonMessage) depends on whether the images are
-// revealed or not.
+This componenet has two states: imagesRevealed & viewButtonMessage.
+An image set is either expanded (imagesRevealed: true) or collapsed (imagesRevealed: false)
+The button message (viewButtonMessage) depends on whether the images are
+revealed or not.
+*/
 
 export default function CuratedSetsImageGallery(props){
 
@@ -23,11 +24,11 @@ export default function CuratedSetsImageGallery(props){
 
   function toggleCuratedSetImages(set, index) {
     console.log("toggle curated set images for: ", set, index);
-    props.imagesRevealed
+    imagesRevealed
       ? setImagesRevealed(false)
       : setImagesRevealed(true);
-    console.log("props.imagesRevealed is ", props.imagesRevealed);
-    props.imagesRevealed ? setViewButtonMessage("view set") : setViewButtonMessage("hide set");
+    console.log("imagesRevealed is ", imagesRevealed);
+    imagesRevealed ? setViewButtonMessage("view set") : setViewButtonMessage("hide set");
   }
 
 
@@ -41,25 +42,19 @@ export default function CuratedSetsImageGallery(props){
         <section
           className="curated-images-gallery"
           id={"curated-set-gallery-" + index}
-          style={{ maxHeight: props.imagesRevealed ? "4000px" : 0 }}
+          style={{ maxHeight: imagesRevealed ? "4000px" : 0 }}
         >
           {allTheSetImagesMinusCover.map((item, index) => {
-            // console.log("allTheSetImages item.id:", item.id)
-            // console.log("allTheSetImages item.title:", item.title)
-            // console.log("allTheSetImages item.url:", item.url)
-            // console.log("allTheSetImages item.imageURL:", item.imageURL)
 
             return (
               <div
                 key={index}
                 id={"-curated-image-" + index}
                 className="curated-image"
-                // style={{display: props.imagesRevealed ? 'block': 'none'}}
               >
                 <a href={item.url}>
                   <img
                     className="curated-list-img"
-                    // src={item.imageURL}
                     src={item.localImageURL}
                     alt={item.title}
                   />
@@ -79,7 +74,6 @@ export default function CuratedSetsImageGallery(props){
           }}
           onClick={(event, index) => {
             toggleCuratedSetImages(thisCuratedSet, index);
-            // let imagesRevealed = props.imagesRevealed
           }}
         >
           {viewButtonMessage}
