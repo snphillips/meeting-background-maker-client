50fdf1f605bb8e0d0a9b839b23123488dfd12a87
Sarah Phillips <snphillips@gmail.com>
2022-11-08 07:49:17 -0500

    remove unused code

--- src/components/DownloadButton.js
+++ src/components/DownloadButton.js
@@ -1,13 +1,11 @@
 import React from "react";
 
-export default function DownloadButton(props) {
+export default function DownloadButton() {
   return (
-      <button className="download-button">
-        <a
-          href="/meeting-backgrounds/arches.jpeg"
-          download="arches.jpeg">
-            Download Set of Selected Images
-        </a>
-      </button>
+    <button className="download-button">
+      <a href="/meeting-backgrounds/arches.jpeg" download="arches.jpeg">
+        Download Set of Selected Images
+      </a>
+    </button>
   );
 }
