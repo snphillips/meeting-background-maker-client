d41cf0e8f18e2f4c22e1a1f163726efbf4aa486a
Sarah Phillips <snphillips@gmail.com>
2026-05-30 11:08:07 -0400

    update serverURL

--- src/App.tsx
+++ src/App.tsx
@@ -28,9 +28,9 @@ const curatedSetsArray = [
 
 export default function App() {
   // const serverURL = `http://localhost:3001/`;
-  // const serverURL = `https://meeting-background-server.herokuapp.com/`;
   // const serverURL = `https://meeting-background-maker-server.netlify.app/api/`;
-  const serverURL = `https://meeting-background-maker-server.netlify.app/.netlify/functions/api/`;
+  // const serverURL = `https://meeting-background-maker-server.netlify.app/.netlify/functions/api/`;
+  const serverURL = `https://meeting-background-maker-server.netlify.app/api/`;
   const imgBucketURL = 'https://meeting-background-maker.s3.amazonaws.com/meeting-backgrounds/';
 
   const initialRender = useRef(true);
